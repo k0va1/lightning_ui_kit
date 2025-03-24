cb18616ac4c2b9759c37ddb3a616922a055cb694
Alex Koval <al3xander.koval@gmail.com>
2025-03-24 12:22:56 +0100

    add pagination component

---
+++ app/components/lightning_ui/pagination_component.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class LightningUi::PaginationComponent < LightningUi::BaseComponent
+  def initialize(current_page:, total_pages:, path:, page_param: "page", with_arrows: false, **options)
+    @current_page = current_page
+    @total_pages = total_pages
+    @with_arrows = with_arrows
+    @path = path
+    @page_param = page_param
+    @options = options
+  end
+
+  def data(disabled: false, active: false)
+    {}.tap do |data|
+      data[:disabled] = true if disabled
+      data[:active] = true if active
+    end
+  end
+
+  def link_classes
+    "min-w-[2.25rem] flex items-center justify-center rounded-lg border text-base/6 font-semibold px-[calc(--spacing(3.5)-1px)]\
+      py-[calc(--spacing(2.5)-1px)] sm:px-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(1.5)-1px)] sm:text-sm/6 \
+      focus:outline-hidden focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-blue-500\
+      data-disabled:opacity-50 data-disabled:pointer-events-none border-transparent text-zinc-950 data-active:bg-zinc-950/5 hover:bg-zinc-950/5"
+  end
+
+  def pages_with_gaps
+    return (1..@total_pages).to_a if @total_pages <= 7
+
+    (1..@total_pages).to_a.each_with_object([]) do |page, pages|
+      if page == 1 || page == @total_pages || page == @current_page || (page - @current_page).abs <= 2
+        pages << page
+      elsif pages.last != :gap
+        pages << :gap
+      end
+    end
+  end
+
+  def url(page)
+    "#{@path}?#{@page_param}=#{page}"
+  end
+end

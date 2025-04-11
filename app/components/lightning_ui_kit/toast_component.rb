a68d0e0a99cff06f73fdef4c97f24bd54365d202
Alex Koval <al3xander.koval@gmail.com>
2025-04-11 14:47:37 +0200

    allow to add custom classes to toast

--- app/components/lightning_ui_kit/toast_component.rb
+++ app/components/lightning_ui_kit/toast_component.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class LightningUiKit::ToastComponent < LightningUiKit::BaseComponent
-  def initialize(autodismiss: true, dismiss_after: 3000)
+  def initialize(autodismiss: true, dismiss_after: 3000, **options)
     @autodismiss = autodismiss
     @dismiss_after = dismiss_after
+    @options = options
+  end
+
+  def classes
+    merge_classes(["lui:w-full lui:flex lui:justify-center lui:fixed lui:bottom-5", @options[:class]].compact.join(" "))
   end
 end

bfb58f8b6dcb51e8cb6b9e580d09e649ccc60650
Alex Koval <al3xander.koval@gmail.com>
2025-10-25 17:45:55 +0200

    fix: improve positioning

--- app/components/lightning_ui_kit/dropdown_component.rb
+++ app/components/lightning_ui_kit/dropdown_component.rb
@@ -11,7 +11,7 @@ class LightningUiKit::DropdownComponent < LightningUiKit::BaseComponent
   end
 
   def menu_classes
-    classes = %w[lui:hidden lui:transition lui:transform lui:p-1 lui:origin-top-left lui:absolute lui:left-0 lui:w-56 lui:rounded-md lui:shadow-lg lui:bg-white lui:ring-1 lui:ring-zinc-950/10 lui:focus:outline-none]
+    classes = %w[lui:hidden lui:transition lui:transform lui:p-1 lui:origin-top-left lui:absolute lui:left-0 lui:rounded-md lui:shadow-lg lui:bg-white lui:ring-1 lui:ring-zinc-950/10 lui:focus:outline-none lui:z-50]
     case @position.to_s
     when "top"
       classes << "lui:mb-2 lui:top-auto lui:bottom-full"

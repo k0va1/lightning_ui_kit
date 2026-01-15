e3b907972e3fe8883032d3049aa1834fae6d19d8
Alex Koval <al3xander.koval@gmail.com>
2026-01-15 18:04:22 +0100

    chore: deploy fix v4

--- lookbook/config/initializers/lookbook.rb
+++
@@ -1,7 +0,0 @@
-# Fix Lookbook asset routes when mounted at a subpath
-# This ensures asset routes match before the catch-all 404 handler
-Rails.application.config.after_initialize do
-  Lookbook::Engine.routes.prepend do
-    get "lookbook-assets/*path", to: "assets#show", as: :lookbook_asset_override
-  end
-end

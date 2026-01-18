1d9ab245ead84c1c3d9ad131190fdcc66e6c0d66
Alex Koval <al3xander.koval@gmail.com>
2026-01-18 21:41:13 +0100

    feat: add icon support to ButtonComponent
    - Add icon, icon_position, and icon_variant parameters
    - Support leading (default) and trailing icon positions
    - Support outline and solid icon variants via Heroicons
    - Fix destructive button icon color (was red-400, now white)
    - Add Lookbook previews for all icon variations

--- lookbook/app/previews/button_component_preview.rb
+++ lookbook/app/previews/button_component_preview.rb
@@ -46,4 +46,41 @@ class ButtonComponentPreview < Lookbook::Preview
     end
   end
   # @endgroup
+
+  # @group Icons
+
+  def with_leading_icon
+    render LightningUiKit::ButtonComponent.new(icon: "plus") do
+      "Add Item"
+    end
+  end
+
+  def with_trailing_icon
+    render LightningUiKit::ButtonComponent.new(icon: "arrow-right", icon_position: :trailing) do
+      "Next"
+    end
+  end
+
+  def with_solid_icon
+    render LightningUiKit::ButtonComponent.new(icon: "check", icon_variant: :solid) do
+      "Save"
+    end
+  end
+
+  def outline_with_icon
+    render LightningUiKit::ButtonComponent.new(style: :outline, icon: "pencil") do
+      "Edit"
+    end
+  end
+
+  def destructive_with_icon
+    render LightningUiKit::ButtonComponent.new(style: :destructive, icon: "trash") do
+      "Delete"
+    end
+  end
+
+  def icon_only
+    render LightningUiKit::ButtonComponent.new(icon: "plus", icon_variant: :solid)
+  end
+  # @endgroup
 end
